2587aaebef5e099af3e83216f567619101525650
nick <ncichon@gmx.com>
2021-02-25 15:19:54 -0600

    working full stack

--- api/api.py
+++ api/api.py
@@ -1,8 +1,11 @@
-import time
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/time')
+@app.route('/secret')
 def get_current_time():
-    return {'time': time.time()}
+    x = request.args['word']
+    if x == 'abc':
+        return {'status': 1}
+    else:
+        return {'status': 0}
